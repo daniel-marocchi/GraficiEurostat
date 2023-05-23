1f9c2bedf7269ecc016e290dbb5cb236682692c9
daniel-marocchi <daniel.marocchi@marconirovereto.it>
2023-05-24 00:02:06 +0200

    Prima consegna - 24/05

--- script.py
+++ script.py
@@ -1,106 +1,113 @@
+#Import
 from dash import Dash, dcc, html, Input, Output, dash_table
 from urllib.request import urlopen
 import json
 import pandas as pd
+import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objects as go
-# pycountry
-#https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson
+pio.templates.default = "plotly_dark"
+
+with urlopen('https://raw.githubusercontent.com/daniel-marocchi/GraficiEurostat/main/EuropeGeoJSON') as response:
+  EuropeGeoJSON = json.load(response)
+dati = pd.read_csv("https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/HSW_N2_02/?format=SDMX-CSV&lang=en&label=label_only")
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 
 # APP
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#external_stylesheets=external_stylesheets
-app = Dash("GraficiEurostat")
+app = Dash("GraficiEurostat", external_stylesheets=external_stylesheets)
 
 
-# Import Dati
-with urlopen('https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson') as response:
-  eu = json.load(response)
-
-#print(eu["features"][2]['properties']['NAME'])
-
-
-dati = (
-	pd.read_csv(
-		"https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/HSW_N2_02$DEFAULTVIEW/?format=SDMX-CSV&lang=en&label=label_only"
-	)
-	.query("nace_r2 == 'Total - all NACE activities'")
-	.query("unit == 'Number'")
-)
-
-dati2 = pd.read_csv(
-	"https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/HSW_N2_02$DEFAULTVIEW/1.0/A.NR.TOTAL.*?c[geo]=BE,BG,CZ,DK,DE,EE,IE,EL,ES,FR,HR,IT,CY,LV,LT,LU,HU,MT,NL,AT,PL,PT,RO,SI,SK,FI,SE,IS,NO,CH,UK&compress=false&format=csvdata&formatVersion=2.0&c[TIME_PERIOD]=ge:2011+le:2020"
-)
-
+#Modellazione dati
+dati = dati.query("nace_r2 == 'Total - all NACE activities'")
+datiIncidenza = dati
+dati = dati.query("unit == 'Number'")
+dati = dati.drop(columns=['DATAFLOW', 'LAST UPDATE', 'OBS_FLAG', 'freq'])
 dati2020 = dati.query("TIME_PERIOD == 2020")
-dati2019 = dati.query("TIME_PERIOD == 2019")
-dati2018 = dati.query("TIME_PERIOD == 2018")
-dati2017 = dati.query("TIME_PERIOD == 2017")
-dati2016 = dati.query("TIME_PERIOD == 2016")
-dati2015 = dati.query("TIME_PERIOD == 2015")
 
+#-----------------------------------------------------------------------------------------------------------------------------
 
-# Grafici
-g1 = px.scatter(
-	dati2020,
+#Grafici
+graficoPunti = px.scatter(
+	dati,
 	x="geo",
 	y="OBS_VALUE",
+  color="OBS_VALUE",
 	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni"},
+  animation_frame="TIME_PERIOD",
+	animation_group="geo",
+  height=1000
 )
+graficoPunti.update_traces(marker_size=30)
 
-g2 = px.bar(dati2020, x="geo", y="OBS_VALUE")
+staticBarChart = px.bar(
+  dati2020, 
+  x="geo", 
+  y="OBS_VALUE",
+  height=1000,
+  labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"})
 
-g4 = px.choropleth(
-	dati2020.query("geo != 'European Union - 27 countries (from 2020)' "),
+
+globe = px.choropleth(
+	dati2020.query("geo != 'European Union - 27 countries (from 2020)'"),
 	color="OBS_VALUE",
-	locations="geo",
+	geojson=EuropeGeoJSON,
+  locations="geo",
 	hover_name="geo",
 	hover_data="OBS_VALUE",
 	locationmode="country names",
-	height=800,
+	height=2000,
 	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazione"},
 	fitbounds="locations",
 	color_continuous_scale="sunsetdark",
 )
-g4.update_geos(
+
+globe.update_geos(
 	showocean=True,
-	oceancolor="LightBlue",
-	#projection_type="orthographic",
+	oceancolor="Gray",
+	projection_type="orthographic",
 	showcountries=True,
 	countrycolor="Gray",
 )
 
-map = px.choropleth_mapbox(
-	dati2.query("TIME_PERIOD == 2020").query(
-		"geo != 'European Union - 27 countries (from 2020)' "
+
+choroplethMap = px.choropleth_mapbox(
+	dati2020.query(
+		"geo != 'European Union - 27 countries (from 2020)'"
 	),
-  geojson=eu,
+  geojson=EuropeGeoJSON,
 	locations="geo",
   featureidkey="properties.NAME",
-	mapbox_style="carto-positron",
+	mapbox_style="carto-darkmatter",
 	center={"lat": 45.891031, "lon": 10.863817},
-	zoom=4,
-	height=700,
+	zoom=3,
+	height=1300,
 	color="OBS_VALUE",
+  color_continuous_scale="sunsetdark",
+  hover_name="geo",
+  hover_data="OBS_VALUE",
+  labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"}
 )
 
+
 animatedBarChart = px.bar(
-	dati2,
+	dati,
 	x="geo",
 	y="OBS_VALUE",
 	animation_frame="TIME_PERIOD",
 	animation_group="geo",
 	color="OBS_VALUE",
+	height=1300,
 	color_continuous_scale="sunsetdark",
-	height=700,
 	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"},
-	range_y=[0, 800],
+	range_y=[0, 5000],
 	hover_data="OBS_VALUE",
 	hover_name="geo",
 )
 
+
 pieChart = px.pie(
-	dati2,
+	dati.query("TIME_PERIOD == 2020"),
 	names="geo",
 	values="OBS_VALUE",
 	height=1000,
@@ -108,51 +115,54 @@ pieChart = px.pie(
 	hover_name="geo",
 )
 
-table = go.Figure(
-	data=[
-		go.Table(
-			header=dict(
-				values=[["ANNO"], ["NAZIONE"], ["INCIDENTI"]],
-				fill_color="paleturquoise",
-				align="center",
-				font=dict(size=18),
-				height=60,
-				),
-				cells=dict(
-				values=[dati2020.TIME_PERIOD, dati2020.geo, dati2020.OBS_VALUE],
-				# fill_color='lavender',
-				align="left",
-				height=40,
-				font_size=20,
-			),
-		)
-	]
-)
-
 pieChart.update_traces(textposition="inside", textinfo="text+label")
 
 
-# LAYOUT
+#App Layout
 app.layout = html.Div(
 	style={
 		"textAlign": "center",
+    #'boxSizing': 'borderBox',
+    'margin' : 0,
+    'padding' : '20px',
+    'color' : 'white',
+    'backgroundColor' : '#111111'
 	},
 	children=[
-		dcc.Graph(figure=table),
+    html.H1('Incidenti sul Lavoro'),
 		dash_table.DataTable(
 			data=dati2020.to_dict("records"),
-			columns=[{"id": c, "name": c} for c in dati2020.columns],
+			columns=[{"id": c, "name": c} for c in dati2020.drop(columns=['nace_r2', 'unit']).columns],
+      style_header={
+        'backgroundColor': 'white',
+        'fontWeight': 'bold',
+        'fontSize' : 35,
+        'textTransform' : 'upperCase',
+        'padding' : '16px 0px',
+        'color' : 'd0d0d0',
+        'backgroundColor' : '#1c1c1c',
+        'borderTop': 'none'
+    },
+      style_cell={'textAlign': 'center', 
+        'fontSize' : 20,
+        'fontFamily' : 'Arial',
+        'padding' : '10px 0px',
+        'backgroundColor' : '#111111'
+      },
+      #style_as_list_view=True,
+      style_table={
+        'margin' : '20px 0px'
+      }
 		),
-		dcc.Graph(figure=g1),
-		dcc.Graph(figure=g2),
-		dcc.Graph(figure=g4),
-		dcc.Graph(figure=map),
+		
+    dcc.Loading(dcc.Graph(figure=animatedBarChart)),
+		dcc.Graph(figure=graficoPunti),
+		dcc.Graph(figure=staticBarChart),
+		dcc.Graph(figure=choroplethMap),
+		dcc.Graph(figure=globe),
 
-		dcc.Loading(dcc.Graph(figure=animatedBarChart)),
-		dcc.Graph(
-			figure=pieChart,
-		),
+		dcc.Graph(figure=pieChart),
 	],
 )
 
-app.run(debug=True)
+app.run(debug=True, dev_tools_hot_reload=True, dev_tools_ui=True)
