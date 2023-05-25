e22a5e4c19a81d32abcb786c155250ae806cb4f1
daniel-marocchi <daniel.marocchi@marconirovereto.it>
2023-05-25 23:16:04 +0200

    Fine - In Teoria...

--- script.py
+++ script.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 pio.templates.default = "plotly_dark"
 
+
 with urlopen('https://raw.githubusercontent.com/daniel-marocchi/GraficiEurostat/main/EuropeGeoJSON') as response:
   EuropeGeoJSON = json.load(response)
 dati = pd.read_csv("https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/HSW_N2_02/?format=SDMX-CSV&lang=en&label=label_only")
@@ -23,146 +24,229 @@ dati = dati.query("nace_r2 == 'Total - all NACE activities'")
 datiIncidenza = dati
 dati = dati.query("unit == 'Number'")
 dati = dati.drop(columns=['DATAFLOW', 'LAST UPDATE', 'OBS_FLAG', 'freq'])
-dati2020 = dati.query("TIME_PERIOD == 2020")
+dati.columns=['Unità', 'Attività', 'Nazione', 'Anno','Numero Incidenti']
+dati2020 = dati.query("Anno == 2020")
 
 #-----------------------------------------------------------------------------------------------------------------------------
 
-#Grafici
+#Grafici Animati
+
 graficoPunti = px.scatter(
 	dati,
-	x="geo",
-	y="OBS_VALUE",
-  color="OBS_VALUE",
-	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni"},
-  animation_frame="TIME_PERIOD",
-	animation_group="geo",
-  height=1000
+	x="Nazione",
+	y="Numero Incidenti",
+  color="Numero Incidenti",
+  animation_frame="Anno",
+	animation_group="Nazione",
+  height=1000,
+  color_continuous_scale="sunsetdark"
 )
 graficoPunti.update_traces(marker_size=30)
 
-staticBarChart = px.bar(
-  dati2020, 
-  x="geo", 
-  y="OBS_VALUE",
-  height=1000,
-  labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"})
-
-
-globe = px.choropleth(
-	dati2020.query("geo != 'European Union - 27 countries (from 2020)'"),
-	color="OBS_VALUE",
-	geojson=EuropeGeoJSON,
-  locations="geo",
-	hover_name="geo",
-	hover_data="OBS_VALUE",
-	locationmode="country names",
-	height=2000,
-	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazione"},
-	fitbounds="locations",
-	color_continuous_scale="sunsetdark",
-)
-
-globe.update_geos(
-	showocean=True,
-	oceancolor="Gray",
-	projection_type="orthographic",
-	showcountries=True,
-	countrycolor="Gray",
-)
-
-
-choroplethMap = px.choropleth_mapbox(
-	dati2020.query(
-		"geo != 'European Union - 27 countries (from 2020)'"
-	),
-  geojson=EuropeGeoJSON,
-	locations="geo",
-  featureidkey="properties.NAME",
-	mapbox_style="carto-darkmatter",
-	center={"lat": 45.891031, "lon": 10.863817},
-	zoom=3,
-	height=1300,
-	color="OBS_VALUE",
-  color_continuous_scale="sunsetdark",
-  hover_name="geo",
-  hover_data="OBS_VALUE",
-  labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"}
-)
-
-
+#Grafico a Barre Animato
 animatedBarChart = px.bar(
 	dati,
-	x="geo",
-	y="OBS_VALUE",
-	animation_frame="TIME_PERIOD",
-	animation_group="geo",
-	color="OBS_VALUE",
+	x="Nazione",
+	y="Numero Incidenti",
+	animation_frame="Anno",
+	animation_group="Nazione",
+	color="Numero Incidenti",
 	height=1300,
 	color_continuous_scale="sunsetdark",
-	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazioni", "TIME_PERIOD": "Anno"},
 	range_y=[0, 5000],
-	hover_data="OBS_VALUE",
-	hover_name="geo",
+	hover_data="Numero Incidenti",
+	hover_name="Nazione",
 )
 
 
-pieChart = px.pie(
-	dati.query("TIME_PERIOD == 2020"),
-	names="geo",
-	values="OBS_VALUE",
-	height=1000,
-	labels={"OBS_VALUE": "Numero Incidenti", "geo": "Nazione"},
-	hover_name="geo",
-)
-
-pieChart.update_traces(textposition="inside", textinfo="text+label")
+#-----------------------------------------------------------------------------------------------------------------------------
 
 
 #App Layout
 app.layout = html.Div(
-	style={
-		"textAlign": "center",
-    #'boxSizing': 'borderBox',
-    'margin' : 0,
-    'padding' : '20px',
-    'color' : 'white',
-    'backgroundColor' : '#111111'
-	},
 	children=[
-    html.H1('Incidenti sul Lavoro'),
+    html.H1('Incidenti sul Lavoro', style={
+      'textAlign' : 'center',
+      'fontSize' : '8rem',
+      'padding' : '3rem',
+      'marginBottom' : '6rem'
+    }),
+    dcc.Slider(
+      dati['Anno'].min(),
+      dati['Anno'].max(),
+      step=1,
+      id='yearSlider',
+      tooltip={
+        'always_visible':True 
+      },
+      value=dati["Anno"].max(),
+      marks={str(year): str(year) for year in dati['Anno'].unique()},
+    ),
+		
 		dash_table.DataTable(
-			data=dati2020.to_dict("records"),
-			columns=[{"id": c, "name": c} for c in dati2020.drop(columns=['nace_r2', 'unit']).columns],
+      columns=[{"id": c, "name": c} for c in dati.drop(columns=['Attività', 'Unità', 'Anno']).columns],
       style_header={
-        'backgroundColor': 'white',
-        'fontWeight': 'bold',
         'fontSize' : 35,
+        'fontWeight': '300',
         'textTransform' : 'upperCase',
         'padding' : '16px 0px',
-        'color' : 'd0d0d0',
         'backgroundColor' : '#1c1c1c',
-        'borderTop': 'none'
-    },
+      },
       style_cell={'textAlign': 'center', 
         'fontSize' : 20,
         'fontFamily' : 'Arial',
         'padding' : '10px 0px',
         'backgroundColor' : '#111111'
       },
-      #style_as_list_view=True,
       style_table={
-        'margin' : '20px 0px'
-      }
-		),
-		
-    dcc.Loading(dcc.Graph(figure=animatedBarChart)),
-		dcc.Graph(figure=graficoPunti),
-		dcc.Graph(figure=staticBarChart),
-		dcc.Graph(figure=choroplethMap),
-		dcc.Graph(figure=globe),
-
-		dcc.Graph(figure=pieChart),
-	],
+        'margin' : '10rem 0px',
+      },
+      #style_as_list_view=True,
+      id='table'),
+		dcc.Graph(id='staticBarChart', style={
+      'margin' : '10rem 0px'
+    }),
+		dcc.Graph(id='pieChart'),
+		dcc.Graph(id='choroplethMapbox', style={
+      'margin' : '10rem 0px'
+    }),
+		dcc.Graph(id='globe', style={
+      'margin' : '10rem 0px'
+    }),
+    dcc.Graph(figure=animatedBarChart, style={
+      'margin' : '10rem 0px'
+    }),
+		dcc.Graph(figure=graficoPunti, style={
+      'margin' : '10rem 0px'
+    })
+	]
 )
 
-app.run(debug=True, dev_tools_hot_reload=True, dev_tools_ui=True)
+#-----------------------------------------------------------------------------------------------------------------------------
+
+#Funzioni di Callback
+#Aggiorno Grafico a Barre Statico
+@app.callback(
+  Output('staticBarChart', 'figure'),
+  Input('yearSlider', 'value')
+)
+def updateStaticBarChart(annoSelezionato):
+  datiFiltrati = dati.query('Anno == '+ str(annoSelezionato))
+  
+  staticBarChart = px.bar(
+    data_frame=datiFiltrati, 
+    x="Nazione", 
+    y="Numero Incidenti",
+    height=1000,
+    hover_name='Nazione',
+    hover_data=['Numero Incidenti', 'Anno'],
+    color_continuous_scale="sunsetdark",
+    color='Numero Incidenti',
+  )
+
+  staticBarChart.update_layout(transition_duration=500)
+
+  return staticBarChart
+
+#Aggiorno Tabella
+@app.callback(
+  Output('table', 'data'),
+  Input('yearSlider', 'value')
+)
+def updateTable(annoSelezionato):
+  datiFiltrati = dati.query('Anno == '+ str(annoSelezionato))
+  data=datiFiltrati.to_dict("records")
+
+  return data
+
+
+#Update Grafico a Torta
+@app.callback(
+  Output('pieChart', 'figure'),
+  Input('yearSlider', 'value')
+)
+def updatePieChart(annoSelezionato):
+  datiFiltrati = dati.query('Anno == '+ str(annoSelezionato))
+  pieChart = px.pie(
+    datiFiltrati,
+    names="Nazione",
+    values="Numero Incidenti",
+    height=1000,
+    hover_name="Nazione",
+    hover_data=['Numero Incidenti']
+  )
+
+  pieChart.update_traces(textposition="inside", textinfo="text+label")
+
+  return pieChart
+
+
+#Update Globo
+@app.callback(
+  Output('globe', 'figure'),
+  Input('yearSlider', 'value')
+)
+def updateGlobe(annoSelezionato):
+  datiFiltrati = dati.query('Anno == '+ str(annoSelezionato))
+    
+  globe = px.choropleth(
+    datiFiltrati
+    .query("Nazione != 'European Union - 210 countries (from 2020)'")
+    .query("Nazione != 'European Union - 28 countries (2013-2020)'")
+    .query("Nazione != 'European Union - 210 countries (20010-2013)'")
+    .query("Nazione != 'European Union - 15 countries (1995-2004)'"),
+    color="Numero Incidenti",
+    geojson=EuropeGeoJSON,
+    locations="Nazione",
+    hover_name="Nazione",
+    hover_data=['Numero Incidenti', 'Anno'],
+    locationmode="country names",
+    height=1800,
+    fitbounds="locations",
+    color_continuous_scale="sunsetdark",
+    )
+
+  globe.update_geos(
+    showocean=True,
+    oceancolor="Gray",
+    projection_type="orthographic",
+    showcountries=True,
+    countrycolor="Gray",
+  )
+
+  globe.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+  return globe
+
+
+#Update Mappa
+@app.callback(
+  Output('choroplethMapbox', 'figure'),
+  Input('yearSlider', 'value')
+)
+def updateChoroplethMapbox(annoSelezionato):
+  datiFiltrati = dati.query('Anno == '+ str(annoSelezionato))
+  
+  choroplethMap = px.choropleth_mapbox(
+	datiFiltrati
+    .query("Nazione != 'European Union - 210 countries (from 2020)'")
+    .query("Nazione != 'European Union - 28 countries (2013-2020)'")
+    .query("Nazione != 'European Union - 210 countries (20010-2013)'")
+    .query("Nazione != 'European Union - 15 countries (1995-2004)'"),
+  geojson=EuropeGeoJSON,
+	locations="Nazione",
+  featureidkey="properties.NAME",
+	mapbox_style="carto-darkmatter",
+	center={"lat": 45.891031, "lon": 10.8638110},
+	zoom=3,
+	height=1300,
+	color="Numero Incidenti",
+  color_continuous_scale="sunsetdark",
+  hover_name="Nazione",
+  hover_data=['Numero Incidenti', 'Anno'],
+  ) 
+
+  return choroplethMap
+
+
+app.run(debug=True)
